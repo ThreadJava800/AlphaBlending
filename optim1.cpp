c4c5213d583e674be0d01eafdc11684d2d358987
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-04-06 18:37:05 +0300

    Optimisation 1 added

--- optim1.cpp
+++ optim1.cpp
@@ -76,18 +76,90 @@ void imposePics(sf::Image* top, sf::Image* back, int backStartX, int backStartY)
     int sizeY = top->getSize().y;
     int sizeX = top->getSize().x;
 
+    __m256 arr255 = _mm256_set1_ps(255);
+    __m256 arr1   = _mm256_set1_ps(1);
+
     for (int i = 0; i < sizeY; i++) {
-        for (int j = 0; j < sizeX; j++) {
-            int alpha = top->getPixel(j, i).a / 255;
+        for (int j = 0; j < sizeX - 7; j += 8) {
             int backX = j + backStartX;
             int backY = i + backStartY;
 
-            back->setPixel(backX, backY, sf::Color(
-                top->getPixel(j, i).r * alpha + (1 - alpha) * back->getPixel(backX, backY).r,
-                top->getPixel(j, i).g * alpha + (1 - alpha) * back->getPixel(backX, backY).g,
-                top->getPixel(j, i).b * alpha + (1 - alpha) * back->getPixel(backX, backY).b,
-                255
-            ));
+            __m256 topAlpha = _mm256_set_ps(
+                    top->getPixel(j + 7, i).a, top->getPixel(j + 6, i).a,
+                    top->getPixel(j + 5, i).a, top->getPixel(j + 4, i).a,
+                    top->getPixel(j + 3, i).a, top->getPixel(j + 2, i).a,
+                    top->getPixel(j + 1, i).a, top->getPixel(j, i).a
+                );
+
+            topAlpha = _mm256_div_ps(topAlpha, arr255);
+
+            __m256 topRed = _mm256_set_ps(
+                    top->getPixel(j + 7, i).r, top->getPixel(j + 6, i).r,
+                    top->getPixel(j + 5, i).r, top->getPixel(j + 4, i).r,
+                    top->getPixel(j + 3, i).r, top->getPixel(j + 2, i).r,
+                    top->getPixel(j + 1, i).r, top->getPixel(j, i).r
+                );
+
+            __m256 topGreen = _mm256_set_ps(
+                    top->getPixel(j + 7, i).g, top->getPixel(j + 6, i).g,
+                    top->getPixel(j + 5, i).g, top->getPixel(j + 4, i).g,
+                    top->getPixel(j + 3, i).g, top->getPixel(j + 2, i).g,
+                    top->getPixel(j + 1, i).g, top->getPixel(j, i).g
+                );
+
+            __m256 topBlue = _mm256_set_ps(
+                    top->getPixel(j + 7, i).b, top->getPixel(j + 6, i).b,
+                    top->getPixel(j + 5, i).b, top->getPixel(j + 4, i).b,
+                    top->getPixel(j + 3, i).b, top->getPixel(j + 2, i).b,
+                    top->getPixel(j + 1, i).b, top->getPixel(j, i).b
+                );
+
+
+            __m256 backRed = _mm256_set_ps(
+                    back->getPixel(backX + 7, backY).r, back->getPixel(backX + 6, backY).r,
+                    back->getPixel(backX + 5, backY).r, back->getPixel(backX + 4, backY).r,
+                    back->getPixel(backX + 3, backY).r, back->getPixel(backX + 2, backY).r,
+                    back->getPixel(backX + 1, backY).r, back->getPixel(backX, backY).r
+                );
+
+            __m256 backGreen = _mm256_set_ps(
+                    back->getPixel(backX + 7, backY).g, back->getPixel(backX + 6, backY).g,
+                    back->getPixel(backX + 5, backY).g, back->getPixel(backX + 4, backY).g,
+                    back->getPixel(backX + 3, backY).g, back->getPixel(backX + 2, backY).g,
+                    back->getPixel(backX + 1, backY).g, back->getPixel(backX, backY).g
+                );
+
+            __m256 backBlue = _mm256_set_ps(
+                    back->getPixel(backX + 7, backY).b, back->getPixel(backX + 6, backY).b,
+                    back->getPixel(backX + 5, backY).b, back->getPixel(backX + 4, backY).b,
+                    back->getPixel(backX + 3, backY).b, back->getPixel(backX + 2, backY).b,
+                    back->getPixel(backX + 1, backY).b, back->getPixel(backX, backY).b
+                );
+
+            __m256 topArrR = _mm256_mul_ps(topRed, topAlpha);
+            __m256 topArrG = _mm256_mul_ps(topGreen, topAlpha);
+            __m256 topArrB = _mm256_mul_ps(topBlue, topAlpha);
+
+            topAlpha = _mm256_sub_ps(arr1, topAlpha);       // 1 - alpha
+
+            __m256 backArrR = _mm256_mul_ps(backRed, topAlpha);
+            __m256 backArrG = _mm256_mul_ps(backGreen, topAlpha);
+            __m256 backArrB = _mm256_mul_ps(backBlue, topAlpha);
+
+            __m256 resR     = _mm256_add_ps(topArrR, backArrR);
+            __m256 resG     = _mm256_add_ps(topArrG, backArrG);
+            __m256 resB     = _mm256_add_ps(topArrB, backArrB);
+
+            float *r          = (float*) (&resR);
+            float *g          = (float*) (&resG);
+            float *b          = (float*) (&resB);
+            
+            for (int m = 0; m < 8; m++, backX++) {
+                back->setPixel(backX, backY, sf::Color(
+                    (unsigned char)r[m], (unsigned char)g[m], (unsigned char)b[m],
+                    255
+                ));
+            }
         }
     }
 }
@@ -125,7 +197,7 @@ void runMainCycle() {
             }
 
             clock_t startTime = clock();
-            imposePics(&catImg, &backImg, 100, 100);
+            imposePics(&catImg, &backImg, 0, 0);
             sprintf(fpsText, "%.2lf ms", ((double)clock() - (double)startTime) / CLOCKS_PER_SEC * 1000);  // ms
             text.setString(fpsText);
 
